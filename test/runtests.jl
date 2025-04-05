b533f1151809c0c0d54ed496d76afb6ebde27537
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-04-05 15:33:20 +0200

    Cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,11 @@
 using OnlineLearningTools: repeat, do_repeat
-using Enzyme
+using Enzyme: gradient, Reverse
 using Test
 
 macro twice(expr)
     return esc(quote
                    @showtime $expr
                    @showtime $expr
-                   @test true
                end)
 end
 
@@ -19,19 +18,7 @@ function fun!(state, params, scratch)
     return nothing # required !
 end
 
-function L2(x, y) # = mapreduce(diff2, +, x,y)
-    ret = zero(eltype(x))
-    for i in eachindex(x, y)
-        @inbounds ret += (x[i] - y[i])^2
-    end
-    return ret
-end
-# L2(x, y) = sum((x - y) .^ 2)
-
-diff2(x, y) = (x - y)^2
-diff2(x::V, y::V) where {V<:Vector} = @inline mapreduce(diff2, +, x, y)
-
-dup(x) = Duplicated(x, make_zero(x))
+L2(x, y) = sum((x - y) .^ 2)
 
 function test_repeat(n=100, nstep=20, target=randn(n), params=rand(n),
                      scratch=zero(params))
@@ -45,30 +32,18 @@ function test_repeat(n=100, nstep=20, target=randn(n), params=rand(n),
         return L2(state, target)
     end
 
-    function grad(loss, state)
-        dstate = make_zero(state)
-        dparams = make_zero(params)
-        autodiff(Reverse, loss, Active, dup(target), Duplicated(state, dstate),
-                 dup(scratch), Duplicated(params, dparams))
-        return dstate, dparams
-    end
-
     repeat(fun!, nstep, target, scratch, params)
 
     state0 = randn(n)
     @twice loss_auto(target, copy(state0), scratch, params)
     @twice grads_auto = gradient(Reverse, loss_auto, target, copy(state0), scratch, params)
-    #    @twice grad(loss_auto, copy(state0))
-
-    #    @twice autodiff(Reverse, repeat, Const, Const(fun!), Const(nstep), dup(copy(state0)), dup(scratch), dup(params))
     @twice loss_custom(target, copy(state0), scratch, params)
-    #    @twice grad(loss_custom, copy(state0))
     @twice grads_custom = gradient(Reverse, loss_custom, target, copy(state0), scratch,
                                    params)
-
-    @test grads_auto[4] ≈ grads_custom[4]
-    @test grads_auto[2] ≈ grads_custom[2]
     @test grads_auto[1] ≈ grads_custom[1]
+    @test grads_auto[2] ≈ grads_custom[2]
+    # we don't care about the gradient w.r.t. scratch
+    @test grads_auto[4] ≈ grads_custom[4]
 end
 
 @testset "OnlineLearningTools.jl" begin
