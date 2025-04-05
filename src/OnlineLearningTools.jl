13b5e84ca63c34c56d648326407b9288b49d10f6
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-04-05 15:29:23 +0200

    v0.1

--- src/OnlineLearningTools.jl
+++ src/OnlineLearningTools.jl
@@ -1,5 +1,81 @@
 module OnlineLearningTools
 
-# Write your package code here.
+using Enzyme: Reverse, Annotation, Const, Duplicated, DuplicatedNoNeed, autodiff 
+
+using Enzyme.EnzymeRules: RevConfig, AugmentedReturn
+import Enzyme.EnzymeRules: reverse, augmented_primal
+
+"""
+    repeat(fun!, n, state, scratch, params)
+
+Equvalent to:
+
+    for i in 1:n
+        fun!(state, scratch, params)
+    end
+
+The following assumptions are made:
+    - `fun!' returns `nothing`
+    - `fun!` may be a closure but it does not contain writable buffers
+    - `state` is both read and modified by `fun!`
+    - `scratch` may be written to by fun! but its initial contents are ignored
+    - `params` are read but not modified by `fun!`
+    - function `rcopy(state)` is implemented correctly.
+
+`repeat` has a custom Enzyme adjoint, whose correctness relies on these assumptions.
+"""
+repeat(fun!, n, state, scratch, params) = do_repeat(fun!, n, state, scratch, params)
+
+# for testing purposes
+function do_repeat(fun!, n, state, scratch, params)
+    for _ in 1:n
+        fun!(state, scratch, params) # modifies state
+    end
+    return nothing
+end
+
+function augmented_primal(::RevConfig,
+                          ::Const{typeof(repeat)},
+                          ::Type{<:Annotation},
+                          fun!::Annotation{<:Function},
+                          n::Annotation,
+                          state::Annotation,
+                          scratch::Annotation,
+                          params::Annotation)
+    st = state.val
+    tape = Vector{typeof(st)}(undef, n.val)
+    for i in 1:(n.val)
+        tape[i] = rcopy(st)
+        fun!.val(st, scratch.val, params.val) # modifies st
+    end
+    return AugmentedReturn(nothing, nothing, tape)
+end
+
+function reverse(::RevConfig,
+                 ::Const{typeof(repeat)},
+                 ::Type{<:Annotation},
+                 tape,
+                 fun!::Annotation{<:Function},
+                 n::Annotation,
+                 state::Annotation,
+                 scratch::Annotation,
+                 params::Annotation)
+    dstate = state.dval
+    for i in (n.val):-1:1
+        autodiff(Reverse, fun!, DuplicatedNoNeed(tape[i], dstate), scratch, params)
+    end
+    return nothing, nothing, nothing, nothing, nothing
+end
+
+"""
+    c = rcopy(x)
+
+Return a copy of `x` that will not be modified if `x` is updated after calling `rcopy`. 
+Acts recursively on (named) tuples, and calls `Base.copy` otherwise.
+`rcopy` is used to store the history of the `state` argument of `repeat` 
+and may need to be specialized for user-defined types.
+"""
+rcopy(x) = copy(x)
+rcopy(x::Union{<:Tuple,<:NamedTuple}) = map(rcopy, x)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,76 @@
-using OnlineLearningTools
+using OnlineLearningTools: repeat, do_repeat
+using Enzyme
 using Test
 
+macro twice(expr)
+    return esc(quote
+                   @showtime $expr
+                   @showtime $expr
+                   @test true
+               end)
+end
+
+# our discrete-time dynamical system
+# we mostly want it to remain bounded when iterating many times
+# so we just let params be random numbers ∈ [0,1]
+function fun!(state, params, scratch)
+    @. scratch = params * state
+    @. state = scratch
+    return nothing # required !
+end
+
+function L2(x, y) # = mapreduce(diff2, +, x,y)
+    ret = zero(eltype(x))
+    for i in eachindex(x, y)
+        @inbounds ret += (x[i] - y[i])^2
+    end
+    return ret
+end
+# L2(x, y) = sum((x - y) .^ 2)
+
+diff2(x, y) = (x - y)^2
+diff2(x::V, y::V) where {V<:Vector} = @inline mapreduce(diff2, +, x, y)
+
+dup(x) = Duplicated(x, make_zero(x))
+
+function test_repeat(n=100, nstep=20, target=randn(n), params=rand(n),
+                     scratch=zero(params))
+    function loss_auto(target, state, scratch, params)
+        do_repeat(fun!, nstep, state, scratch, params)
+        return L2(state, target)
+    end
+
+    function loss_custom(target, state, scratch, params)
+        repeat(fun!, nstep, state, scratch, params)
+        return L2(state, target)
+    end
+
+    function grad(loss, state)
+        dstate = make_zero(state)
+        dparams = make_zero(params)
+        autodiff(Reverse, loss, Active, dup(target), Duplicated(state, dstate),
+                 dup(scratch), Duplicated(params, dparams))
+        return dstate, dparams
+    end
+
+    repeat(fun!, nstep, target, scratch, params)
+
+    state0 = randn(n)
+    @twice loss_auto(target, copy(state0), scratch, params)
+    @twice grads_auto = gradient(Reverse, loss_auto, target, copy(state0), scratch, params)
+    #    @twice grad(loss_auto, copy(state0))
+
+    #    @twice autodiff(Reverse, repeat, Const, Const(fun!), Const(nstep), dup(copy(state0)), dup(scratch), dup(params))
+    @twice loss_custom(target, copy(state0), scratch, params)
+    #    @twice grad(loss_custom, copy(state0))
+    @twice grads_custom = gradient(Reverse, loss_custom, target, copy(state0), scratch,
+                                   params)
+
+    @test grads_auto[4] ≈ grads_custom[4]
+    @test grads_auto[2] ≈ grads_custom[2]
+    @test grads_auto[1] ≈ grads_custom[1]
+end
+
 @testset "OnlineLearningTools.jl" begin
-    # Write your tests here.
+    test_repeat()
 end
