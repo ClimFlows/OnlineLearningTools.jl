9d17bf555319361219ada1705e6ced94b7ba1300
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-04-06 11:54:46 +0200

    More robust (and simpler) implementation

--- src/OnlineLearningTools.jl
+++ src/OnlineLearningTools.jl
@@ -1,6 +1,6 @@
 module OnlineLearningTools
 
-using Enzyme: Reverse, Annotation, Const, Duplicated, DuplicatedNoNeed, autodiff 
+using Enzyme: Reverse, Annotation, Const, Duplicated, MixedDuplicated, DuplicatedNoNeed, autodiff 
 
 using Enzyme.EnzymeRules: RevConfig, AugmentedReturn
 import Enzyme.EnzymeRules: reverse, augmented_primal
@@ -13,11 +13,12 @@ Equvalent to:
     for i in 1:n
         fun!(state, scratch, params)
     end
+    return nothing
 
 The following assumptions are made:
     - `fun!` may be a closure but it does not contain writable buffers
     - `state` is both read and modified by `fun!`
-    - `scratch` may be written to by fun! but its initial contents are ignored
+    - `scratch` may be written to by fun! but values passed to `fun!` are ignored
     - `params` are read but not modified by `fun!`
     - function `rcopy(state)` is implemented correctly.
 
@@ -36,7 +37,7 @@ end
 function augmented_primal(::RevConfig,
                           ::Const{typeof(repeat)},
                           ::Type{<:Annotation},
-                          fun!::Annotation{<:Function},
+                          fun!::Annotation,
                           n::Annotation,
                           state::Annotation,
                           scratch::Annotation,
@@ -50,11 +51,19 @@ function augmented_primal(::RevConfig,
     return AugmentedReturn(nothing, nothing, tape)
 end
 
+no_return_(fun!) = function(state, scratch, params)
+    fun!(state, scratch, params)
+    return nothing
+end
+no_return(fun!::Duplicated) = Duplicated(no_return_(fun!.val), no_return_(fun!.dval))
+no_return(fun!::MixedDuplicated) = MixedDuplicated(no_return_(fun!.val), Base.RefValue(no_return_(fun!.dval[])))
+no_return(fun!::Const) = Const(no_return_(fun!.val))
+
 function reverse(::RevConfig,
                  ::Const{typeof(repeat)},
                  ::Type{<:Annotation},
                  tape,
-                 fun!::Annotation{<:Function},
+                 fun!::Annotation,
                  n::Annotation,
                  state::Annotation,
                  scratch::Annotation,
@@ -67,7 +76,9 @@ function reverse(::RevConfig,
 
     dstate = state.dval
     for i in (n.val):-1:1
-        autodiff(Reverse, f!, DuplicatedNoNeed(tape[i], dstate), scratch, params)
+#        autodiff(Reverse, Const(f!), DuplicatedNoNeed(tape[i], dstate), scratch, params)
+#        autodiff(Reverse, no_return(fun!), DuplicatedNoNeed(tape[i], dstate), scratch, params)
+        autodiff(Reverse, fun!, Const, DuplicatedNoNeed(tape[i], dstate), scratch, params)
     end
     return nothing, nothing, nothing, nothing, nothing
 end
